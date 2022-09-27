cc271594da9bf01a6a072b56d976eea9c5f1535b
Christy <christyxiu@gmail.com>
2022-09-27 14:13:16 -0700

    startdate

--- backend/db/seeders/20220927052233-bookings.js
+++ backend/db/seeders/20220927052233-bookings.js
@@ -15,33 +15,33 @@ module.exports = {
       {
         spotId: 1,
         userId: 1,
-        startDate: 2022-5-1,
-        endDate: 2022-5-5
-      },
-      {
-        spotId: 2,
-        userId: 1,
-        startDate: 2022-6-1,
-        endDate: 2022-6-5
-     },
-     {
-        spotId: 3,
-        userId: 5,
-        startDate: 2022-7-1,
-        endDate: 2022-7-5
-      },
-      {
-        spotId: 4,
-        userId: 5,
-        startDate: 2022-5-12,
-        endDate: 2022-5-14
-      },
-      {
-        spotId: 5,
-        userId: 5,
-        startDate: 2022-8-12,
-        endDate: 2022-8-14
-     }
+        startDate: "2022-5-1 4:7:31",
+        endDate: "2022-5-5 4:7:31"
+      }
+    //   {
+    //     spotId: 2,
+    //     userId: 1,
+    //     startDate: 2022-6-1,
+    //     endDate: 2022-6-5
+    //  },
+    //  {
+    //     spotId: 3,
+    //     userId: 5,
+    //     startDate: 2022-7-1,
+    //     endDate: 2022-7-5
+    //   },
+    //   {
+    //     spotId: 4,
+    //     userId: 5,
+    //     startDate: 2022-5-12,
+    //     endDate: 2022-5-14
+    //   },
+    //   {
+    //     spotId: 5,
+    //     userId: 5,
+    //     startDate: 2022-8-12,
+    //     endDate: 2022-8-14
+    //  }
       ], {});
   },
 
